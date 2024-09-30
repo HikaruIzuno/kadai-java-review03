586d49adc43669e040e5181f624385d0c98faf9b
HikaruIzuno <izuno.hikaru@linecorp.com>
2024-09-30 15:47:01 +0900

    とりあえずコミット

---
+++ src/ObjectSample.java
@@ -0,0 +1,10 @@
+public class ObjectSample {
+
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.employeeName = "大島";
+        emp.divisionName = "営業部";
+        emp.introduce();
+    }
+
+}
