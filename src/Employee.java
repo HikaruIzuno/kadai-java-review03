21c3ea5d2da205dffa045e00b87c5efc20b58119
HikaruIzuno <izuno.hikaru@linecorp.com>
2024-10-01 12:51:07 +0900

    課題用ver.01

--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,34 @@
 public class Employee {
     // フィールド
-    public String employeeName;     // 社員名
-    public String divisionName;     // 部署名
-    public int vitality;            // 体力
+    public String employeeName; // 社員名
+    public String divisionName; // 部署名
+    public int vitality;        // 体力
+
+    // 引数なしのコンストラクタ
+    public Employee() {
+        this("未設定です");
+        System.out.println("引数なしコンストラクタを終了しました");
+    }
+
+    // String型引数1つがあるコンストラクタ
+    public Employee(String employeeName) {
+        this(employeeName,"営業部");
+        System.out.println("String型引数1つがあるコンストラクタを終了しました");
+    }
+
+    // String型引数2つがあるコンストラクタ
+    public Employee(String employeeName, String divisionName) {
+        this(employeeName,divisionName, 150);
+        System.out.println("String型引数2つがあるコンストラクタを終了しました");
+    }
+
+    // String型引数2つ、int型引数1つあるコンストラクタ
+    public Employee(String employeeName, String divisionName, int vitality) {
+        System.out.println("インスタンス化時に引数ありコンストラクタが呼ばれました");
+        this.employeeName = employeeName;
+        this.divisionName = divisionName;
+        this.vitality = vitality;
+    }
 
     // 自己紹介メソッド
     public void introduce() {
